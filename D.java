c89bd0a53f13f037682205ae79c97783a04883b7
AyoKun <ayanavaghosh_arijit@srmuniv.edu.in>
2017-07-22 10:26:30 +0530

    Update D.java

--- D.java
+++ D.java
@@ -1,1 +1,41 @@
-
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Main{
+  int Input(){
+    int n;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter any Number");
+    n = sc.nextInt();
+    return n;
+  }
+  int comp(int n){
+    int x;
+    if(n>0){
+      x=1;
+    }
+    else if(n<0){
+      x=-1;
+    }
+    else 
+    {
+      x=0;
+    }
+    return x;
+  }
+  public static void main(String args[])throws java.lang.Exception{
+    int n,x;
+    Main ob = new Main();
+    n = ob.input();
+    x = ob.comp(n);
+    if(x>0){
+      System.out.println("Number is positive.");
+    }
+    else if(x<0){
+      System.out.println("Number is negative.");
+    }
+    else{
+      System.out.println("Number is zero.");
+    }
+  }
+}
