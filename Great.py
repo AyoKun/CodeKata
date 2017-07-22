0ca13a585ff4fd8112b9192242e2a6e20ec4869c
AyoKun <ayanavaghosh_arijit@srmuniv.edu.in>
2017-07-22 12:25:55 +0530

    Update Great.py

--- Great.py
+++ Great.py
@@ -1,1 +1,9 @@
-
+n1 = int(input("Enter the first number : "))
+n2 = int(input("Enter the second number : "))
+n3 = int(input("Enter the third number : "))
+grt = n1
+if(n2>grt):
+  grt = n2
+if(n3>grt):
+  grt = n3
+print("The Greatest number : ",grt)
