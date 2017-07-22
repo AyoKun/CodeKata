383117eb69bce1f4fa1b3ddcd137ff66c765eaad
AyoKun <ayanavaghosh_arijit@srmuniv.edu.in>
2017-07-22 13:51:23 +0530

    Update SumN.py

--- SumN.py
+++ SumN.py
@@ -1,1 +1,12 @@
-
+n = int(input("Enter the total number of numbers to be summed : "))
+n1 = n
+s = 0
+i = 1
+if(n>0):
+  while(n1>0):
+    s = s + i
+    n1=n1-1
+    i=i+1
+  print("The sum of ",n," numbers is : ", s)
+else:
+  print("Invalid input.")
